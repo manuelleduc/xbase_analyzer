0834b8393da365be26291dd84cea432a34d35e2e
Manuel Leduc <manuel.leduc@inria.fr>
2018-01-10 17:06:06 +0100

    Extract ecore graphviz report.

---
+++ src/xbase/reports/EcoreGraphvizReport.java
@@ -0,0 +1,42 @@
+package xbase.reports;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.eclipse.emf.ecore.EClass;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+
+import xbase_analyzer.EClassToString;
+
+public class EcoreGraphvizReport {
+
+	public void produceEcoreGraphviz(final DefaultDirectedGraph<EClass, DefaultEdge> graph) throws IOException {
+		final String nl = System.lineSeparator();
+		final StringBuilder sb = new StringBuilder();
+		sb.append("digraph {");
+		sb.append(nl);
+		graph.edgeSet().forEach(e -> {
+			final EClass src = graph.getEdgeSource(e);
+			final EClass tgt = graph.getEdgeTarget(e);
+
+			final EClassToString eClassToString = new EClassToString("_");
+			final String ssrc = eClassToString.apply(src);
+			final String stgt = eClassToString.apply(tgt);
+
+			sb.append(ssrc + " -> " + stgt);
+			sb.append(nl);
+		});
+		sb.append(nl);
+		sb.append("}");
+
+		final BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("result.dot"));
+
+		bufferedWriter.write(sb.toString());
+
+		bufferedWriter.close();
+
+	}
+
+}
--- src/xbase_analyzer/Analyzer.java
+++ src/xbase_analyzer/Analyzer.java
@@ -53,6 +53,7 @@ import com.google.inject.Inject;
 import com.google.inject.Provider;
 
 import xbase.reports.EcoreCSVReport;
+import xbase.reports.EcoreGraphvizReport;
 
 public class Analyzer {
 	public static void main(final String[] args) throws Exception {
@@ -116,7 +117,7 @@ public class Analyzer {
 		epackage.getEClassifiers().forEach(new EClassConsumer(visitedClasses, visitedPackages, graph));
 
 		new EcoreCSVReport().produceEcoreCSV(graph);
-		produceEcoreGraphviz(graph);
+		new EcoreGraphvizReport().produceEcoreGraphviz(graph);
 		produceEcoreSqlite(graph);
 
 	}
@@ -182,35 +183,6 @@ public class Analyzer {
 			Files.delete(file.toPath());
 		}
 	}
-
-	private void produceEcoreGraphviz(final DefaultDirectedGraph<EClass, DefaultEdge> graph) throws IOException {
-		final String nl = System.lineSeparator();
-		final StringBuilder sb = new StringBuilder();
-		sb.append("digraph {");
-		sb.append(nl);
-		graph.edgeSet().forEach(e -> {
-			final EClass src = graph.getEdgeSource(e);
-			final EClass tgt = graph.getEdgeTarget(e);
-
-			final EClassToString eClassToString = new EClassToString("_");
-			final String ssrc = eClassToString.apply(src);
-			final String stgt = eClassToString.apply(tgt);
-
-			sb.append(ssrc + " -> " + stgt);
-			sb.append(nl);
-		});
-		sb.append(nl);
-		sb.append("}");
-
-		final BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("result.dot"));
-
-		bufferedWriter.write(sb.toString());
-
-		bufferedWriter.close();
-
-	}
-
-	
 
 	private ResourceSet initResourceSet() {
 		final ResourceSet set = new XtextResourceSet();
