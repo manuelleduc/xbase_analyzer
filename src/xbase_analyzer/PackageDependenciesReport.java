93e49d13332d26e2753cfe591325240d7bf47727
Manuel Leduc <manuel.leduc@inria.fr>
2018-01-11 10:53:24 +0100

    Produces a dot ecore packages dependency graph from the database produced by the analysis of the ecore files of the studied xtext languages.

---
+++ src/xbase_analyzer/PackageDependenciesReport.java
@@ -0,0 +1,55 @@
+package xbase_analyzer;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Reads the sqlite database produced by @see xbase_analyzer.Analyzer and build
+ * a dot file of the graph dependencies between packages.
+ * 
+ * @author mleduc
+ *
+ */
+public class PackageDependenciesReport {
+	public static void main(final String[] args) throws SQLException, IOException {
+		new PackageDependenciesReport().execute();
+	}
+
+	private void execute() throws SQLException, IOException {
+		final String nl = System.lineSeparator();
+		final Connection connection = DriverManager.getConnection("jdbc:sqlite:result.db");
+		final Statement statement = connection.createStatement();
+
+		final ResultSet rs = statement.executeQuery("SELECT DISTINCT pkgSrc, pkdDst, count(*) " + "FROM dependencies "
+				+ "where pkdDst <> dependencies.pkgSrc and lgt = 1 " + "GROUP BY pkgSrc, pkdDst");
+
+		final StringBuilder sb = new StringBuilder();
+
+		sb.append("digraph {");
+		sb.append(nl);
+		while (rs.next()) {
+			final String column1 = rs.getString(1);
+			final String column2 = rs.getString(2);
+			final int column3 = rs.getInt(3);
+			sb.append(column1);
+			sb.append(" -> ");
+			sb.append(column2);
+			sb.append("\t[penwidth=" + column3 / 10.0 + ", label=" + column3 + ", weight=" + column3 + "]");
+			sb.append(nl);
+		}
+		sb.append("}");
+
+		final FileWriter fw = new FileWriter(new File("packages_dependencies.dot"));
+
+		fw.write(sb.toString());
+
+		fw.close();
+
+	}
+}
